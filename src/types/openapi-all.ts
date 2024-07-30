464e90a4eb0cdb2eff7d5b2f60a7ffc866f17d0a
#云淡然 <hi@ydr.me>
2024-07-30 22:18:08 +0800

    feat: 增加 openapi2.0、3.0、3.1 全类型

---
+++ src/types/openapi-all.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-types */
+
+import * as OpenAPIV2 from './openapi-2_0';
+import * as OpenAPIV3 from './openapi-3_0';
+import * as OpenAPIV3_1 from './openapi-3_1';
+
+export type Document<T extends {} = {}> = OpenAPIV2.Document<T> | OpenAPIV3.Document<T> | OpenAPIV3_1.Document<T>;
+export type Operation<T extends {} = {}> = OpenAPIV2.OperationObject<T> | OpenAPIV3.OperationObject<T> | OpenAPIV3_1.OperationObject<T>;
+export type Parameter =
+    | OpenAPIV3_1.ReferenceObject
+    | OpenAPIV3_1.ParameterObject
+    | OpenAPIV3.ReferenceObject
+    | OpenAPIV3.ParameterObject
+    | OpenAPIV2.ReferenceObject
+    | OpenAPIV2.Parameter;
+export type Parameters =
+    | (OpenAPIV3_1.ReferenceObject | OpenAPIV3_1.ParameterObject)[]
+    | (OpenAPIV3.ReferenceObject | OpenAPIV3.ParameterObject)[]
+    | (OpenAPIV2.ReferenceObject | OpenAPIV2.Parameter)[];
+export interface Request {
+    body?: any;
+    headers?: object;
+    params?: object;
+    query?: object;
+}
--- src/types/openapi.ts
+++ src/types/openapi.ts
@@ -1,1 +1,25 @@
-export type * as OpenApi3 from './openapi-v3';
+export type * as OpenAPIV2 from './openapi-2_0';
+export type * as OpenAPIV3 from './openapi-3_0';
+export type * as OpenAPIV3_1 from './openapi-3_1';
+export type * as OpenAPILatest from './openapi-3_1';
+export type * as OpenAPIAll from './openapi-all';
+
+export enum OpenAPIVersion {
+    /**
+     * OpenAPI 2.0.0
+     * {@link} https://github.com/OAI/OpenAPI-Specification/blob/main/versions/2.0.md
+     */
+    V2_0 = '2.0.0',
+
+    /**
+     * OpenAPI 3.0.3
+     * {@link} https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.0.3.md
+     */
+    V3_0 = '3.0.3',
+
+    /**
+     * OpenAPI 3.1.0
+     * {@link} https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.0.md
+     */
+    V3_1 = '3.1.0',
+}
