321989765b1f32a84fff28c7e588e79bb3bdb145
#云淡然 <hi@ydr.me>
2024-08-06 00:56:27 +0800

    chore: update todo

--- test/printer/upload.test.ts
+++ test/printer/upload.test.ts
@@ -147,3 +147,99 @@ test('upload single', () => {
       }"
     `);
 });
+
+test('upload multiple', () => {
+    const printer = new Printer({
+        openapi: '3.1.0',
+        info: {
+            title: 'test',
+            version: '1.0.0',
+        },
+        paths: {
+            '/upload': {
+                post: {
+                    tags: ['upload'],
+                    summary: 'upload',
+                    description: 'upload',
+                    operationId: 'upload',
+                    parameters: [
+                        {
+                            name: 'category',
+                            in: 'query',
+                            description: 'request param',
+                            required: true,
+                            schema: {
+                                type: 'string',
+                                enum: ['a', 'b'],
+                            },
+                        },
+                    ],
+                    requestBody: {
+                        content: {
+                            'multipart/form-data': {
+                                schema: {
+                                    type: 'object',
+                                    properties: {
+                                        file: {
+                                            type: 'array',
+                                            items: {
+                                                type: 'string',
+                                                format: 'binary',
+                                                description: 'A file',
+                                                required: true,
+                                            },
+                                        },
+                                        name: {
+                                            type: 'string',
+                                            description: 'A name',
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    });
+
+    const output = printer.print({
+        hideComponents: true,
+        hideInfo: true,
+        hideFooters: true,
+        hideHeaders: true,
+        hideImports: true,
+        hideHelpers: true,
+    });
+
+    expect(output).toMatchInlineSnapshot(`
+      "/**
+       * @description upload
+       * @summary upload
+       * @param category request param
+       * @param data request data
+       * @param [config] request config
+       */
+      export async function upload(category:("a"|"b"),data:{
+      "file"?:Array<
+      /**
+       * @description A file
+       * @format binary
+       */
+      Blob
+      >;
+      /**
+       * @description A name
+       */
+      "name"?:string;
+      },config?:AxiosRequestConfig): AxiosPromise<unknown> {
+          return axios({
+              method: "post",
+              url: \`/upload\`,
+      params: {category: category},
+      data: data,
+      ...config
+          });
+      }"
+    `);
+});
