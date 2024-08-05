070e00126a06d55dcf20aedf1c625254af3c7dd5
#云淡然 <hi@ydr.me>
2024-08-05 23:39:05 +0800

    test: 支持 ref.$anchor

--- test/printer/ref-schema.test.ts
+++ test/printer/ref-schema.test.ts
@@ -1,3 +1,4 @@
+import type { Nullable } from 'vitest';
 import { Printer } from '../../src/printer';
 
 test('ref $id', () => {
@@ -62,6 +63,83 @@ test('ref $id', () => {
        */
       "t0"?:AxiosRequestConfig_2;
       "t00"?:AxiosRequestConfig_2;
+      };"
+    `);
+});
+
+test('ref $anchor', () => {
+    const printer = new Printer({
+        openapi: '3.1.0',
+        info: {
+            title: 'Test',
+            version: '1.0.0',
+        },
+        paths: {},
+        components: {
+            schemas: {
+                T0: {
+                    type: 'object',
+                    properties: {
+                        aa: {
+                            type: 'string',
+                            format: 'uuid',
+                            $anchor: 'aa',
+                        },
+                        bb: {
+                            type: 'array',
+                            items: {
+                                type: 'object',
+                                properties: {
+                                    cc: {
+                                        $ref: '#/components/schemas/T0#aa',
+                                    },
+                                    dd: {
+                                        type: 'number',
+                                        $anchor: 'dd',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+                T1: {
+                    type: 'object',
+                    properties: {
+                        aa: {
+                            $ref: '#/components/schemas/T0#aa',
+                        },
+                        dd: {
+                            $ref: '#/components/schemas/T0#dd',
+                        },
+                    },
+                },
+            },
+        },
+    });
+
+    expect(
+        printer.print({
+            hideHeaders: true,
+            hideFooters: true,
+            hideHelpers: true,
+            hideImports: true,
+            hideInfo: true,
+        }),
+    ).toMatchInlineSnapshot(`
+      "export type T0 = {
+      /**
+       * @format uuid
+       */
+      "aa"?:string;
+      "bb"?:{
+      "cc"?:DeepGet<T0, ["aa"]>;
+      "dd"?:number;
+      }[];
+      };
+
+      export type T1 = {
+      "aa"?:DeepGet<T0, ["aa"]>;
+      "dd"?:DeepGet<T0, ["bb", number, "dd"]>;
       };"
     `);
 });
