252d70b703e5d3e721208d71bfd083c4fa554213
☁️淡然 <hi@ydr.me>
2025-01-10 15:46:00 +0800

    fix: 修改配置文件名

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,3 @@
 export const pkgName = PKG_NAME;
 export const pkgVersion = PKG_VERSION;
-export const configFileBaseName = 'openapi.config';
+export const configFileBaseName = 'openapi-axios.config';
